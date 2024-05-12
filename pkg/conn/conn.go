0570958e91d21a46b6dcfc3b29e429f6170baacd
Joan Marc Carbo Arnau <jmcarbo@gmail.com>
2024-05-12 11:14:29 +0200

    Remove 'gorilla.go', update interface in 'conn.go'
    The 'gorilla.go' file has been deleted from the 'pkg/conn/gorilla' directory. Additionally, connection interface in 'conn.go' from the 'pkg/conn' directory has been updated to include Start() method. Also, the functionality to start a database connection has been added in 'db.go'.

--- pkg/conn/conn.go
+++ pkg/conn/conn.go
@@ -13,6 +13,7 @@ var (
 type Connection interface {
 	Connect(url string) (Connection, error)
 	Send(method string, params []interface{}) (interface{}, error)
+	Start() error
 	Close() error
 	LiveNotifications(id string) (chan model.Notification, error)
 }
